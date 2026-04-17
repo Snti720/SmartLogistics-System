c9f8c5b46ba0fa6ad909464f637aa83a727e9e3e
Santiago <santicree720@gmail.com>
2026-04-17 15:36:49 -0300

    Se ha añadido toda la lógica de CentroControl

--- src/CentroControl.java
+++ src/CentroControl.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
 public class CentroControl {
+    private ArrayList<Vehiculo> listaVehiculos;
     public CentroControl(){
-
+        listaVehiculos = new ArrayList<>();
     }
-    //Se necesita Vehiculo para avanzar
+    public void registrarUnidad(Vehiculo v){
+        listaVehiculos.add(v);
+    }
+    public void monitorearFlota(){
+        for(Vehiculo v: listaVehiculos){
+            v.patronMovimiento();
+        }
+    }
 }
